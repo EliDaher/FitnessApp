225a2e449480f3702edcca08d4caf3b0116e673e
EliDaher <eeli56315@gmail.com>
2025-05-13 09:51:35 +0300

    test

--- app/screens/Boarding1.tsx
+++ app/screens/Boarding1.tsx
@@ -113,7 +113,7 @@ export default function Boarding1() {
           <View>
             
           <Animated.Image
-            source={require('../../assets/images/workOut.png')}
+            source={require('../../assets/images/workOut1.png')}
             style={[tw`mx-auto mb-4 ${shownImg == 1 ? `block`: `hidden`}`,
                 { opacity: fadeAnim1 },
 
